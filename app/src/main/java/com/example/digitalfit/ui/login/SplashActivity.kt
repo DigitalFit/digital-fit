6539f56a49d375f8699a41f43caa236c16185d87
Alessandro Machado <aleholiveira@yahoo.com.br>
2021-08-23 22:26:44 -0300

    Alessandro-Telas Login

--- app/src/main/java/com/example/digitalfit/ui/login/SplashActivity.kt
+++ app/src/main/java/com/example/digitalfit/ui/login/SplashActivity.kt
@@ -2,10 +2,15 @@ package com.example.digitalfit.ui.login
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.digitalfit.databinding.ActivitySplashBinding
 
 class SplashActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivitySplashBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_splash)
+        binding = ActivitySplashBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
