d9f8587a0f561a8e48ededb6f7319979ecb2d4e5
Ricardo Tsutsumiuchi <ricardo.ryt@gmail.com>
2021-08-27 17:47:26 -0300

    correção de constraints fragment_workout.xml

--- app/src/main/java/com/example/digitalfit/ui/login/SplashActivity.kt
+++ app/src/main/java/com/example/digitalfit/ui/login/SplashActivity.kt
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.digitalfit.databinding.ActivitySplashBinding
 
+
 class SplashActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivitySplashBinding
